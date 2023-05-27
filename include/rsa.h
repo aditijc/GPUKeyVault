7a6a35ec7a270266448f9883ae327848078c97c2
Saumya-Chauhan-MHC <73683540+Saumya-Chauhan-MHC@users.noreply.github.com>
2023-05-27 02:47:44 -0700

    rsa w/o openssl pushed

--- include/rsa.h
+++ include/rsa.h
@@ -1,7 +1,7 @@
-char *generate_rsa_public_key(void);
+// char *generate_rsa_public_key(void);
 
-char *generate_rsa_private_key(void);
+// char *generate_rsa_private_key(void);
 
-char *encrypt_rsa(char *message, char *public_key);
+// char *encrypt_rsa(char *message, char *public_key);
 
-char *decrypt_rsa(char *message, char *private_key);
+// char *decrypt_rsa(char *message, char *private_key);
